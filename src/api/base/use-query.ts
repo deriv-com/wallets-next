1d2b00e3758975e6b9a4004e79eb16fb6cef80f1
yashim-deriv <yashim@regentmarkets.com>
2023-12-05 21:44:40 +0800

    yashim/feat: implement useQuery (#1)

--- src/api/base/use-query.ts
+++ src/api/base/use-query.ts
@@ -1,3 +1,21 @@
-const useQuery = () => {};
+import { useQuery as _useQuery } from "@tanstack/react-query";
+import { TSocketEndpointNames, TSocketError, TSocketRequestPayload, TSocketResponseData } from "../types/api.types";
+import useDerivAPI from "./use-deriv-api";
+
+type TSocketRequestQuery<T extends TSocketEndpointNames> = Parameters<
+    typeof _useQuery<TSocketResponseData<T>, TSocketError<T>>
+>[0] & {
+    name: T;
+    payload: TSocketRequestPayload<T>;
+};
+
+const useQuery = <T extends TSocketEndpointNames>({ name, payload, queryKey, ...rest }: TSocketRequestQuery<T>) => {
+    const { send } = useDerivAPI();
+    return _useQuery<TSocketResponseData<T>, TSocketError<T>>({
+        queryKey: [name, ...queryKey],
+        queryFn: () => send(name, payload),
+        ...rest,
+    });
+};
 
 export default useQuery;
