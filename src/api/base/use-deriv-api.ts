278a6fce5438d435fec060bf461681c77d7451ee
yashim-deriv <yashim@regentmarkets.com>
2023-12-07 10:47:54 +0800

    yashim/feat: implement subscribe and unsubscribe (#3)

--- src/api/base/use-deriv-api.ts
+++ src/api/base/use-deriv-api.ts
@@ -1,25 +1,12 @@
 import { useContext } from "react";
 import { APIDataContext } from "../context/api-context";
-import { TSocketEndpointNames, TSocketRequestPayload, TSocketResponseData } from "../types/api.types";
 
 const useDerivAPI = () => {
     const context = useContext(APIDataContext);
     if (!context) {
         throw new Error("useAPI() must be used within APIProvider");
     }
-
-    const { derivAPI } = context;
-
-    const send = async <T extends TSocketEndpointNames>(
-        name: T,
-        payload?: TSocketRequestPayload<T>
-    ): Promise<TSocketResponseData<T>> => {
-        return derivAPI?.send({ [name]: 1, ...payload });
-    };
-
-    const subscribe = () => {};
-
-    return { send, subscribe } as const;
+    return context;
 };
 
 export default useDerivAPI;
--- src/api/context/api-context.tsx
+++ src/api/context/api-context.tsx
@@ -3,20 +3,59 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // @ts-expect-error Deriv API is not typed
 import DerivAPI from "@deriv/deriv-api/dist/DerivAPI";
 import { getWebsocketURL } from "../../utils/websocket.utils";
+import {
+    TSocketEndpointNames,
+    TSocketRequestPayload,
+    TSocketResponseData,
+    TSocketSubscribableEndpointNames,
+} from "../types/api.types";
+import { Observable, Subscription } from "rxjs";
 
 const queryClient = new QueryClient();
 
+type TSendFunction = <T extends TSocketEndpointNames>(
+    name: T,
+    payload?: TSocketRequestPayload<T>
+) => Promise<TSocketResponseData<T>>;
+
+type TSubscribeFunction = <T extends TSocketSubscribableEndpointNames>(
+    name: T,
+    payload?: TSocketRequestPayload<T>
+) => { id: string; subscription: Observable<TSocketResponseData<T>> };
+
+type TUnsubscribeFunction = (id: string) => void;
+
 type APIData = {
     derivAPI: DerivAPI;
+    send: TSendFunction;
+    subscribe: TSubscribeFunction;
+    unsubscribe: TUnsubscribeFunction;
 };
 
 export const APIDataContext = createContext<APIData | null>(null);
 
 const APIProvider = ({ children }: PropsWithChildren) => {
     const derivAPI = useRef<DerivAPI>(new DerivAPI({ connection: new WebSocket(getWebsocketURL()) }));
+    const subscriptions = useRef<Record<string, Subscription>>();
+
+    const send: TSendFunction = (name, payload) => {
+        return derivAPI.current?.send({ [name]: 1, ...payload });
+    };
+
+    const subscribe: TSubscribeFunction = (name, payload) => {
+        const id = "";
+        const subscription = derivAPI.current?.subscribe({ [name]: 1, subscribe: 1, ...(payload || {}) });
+
+        return { id, subscription };
+    };
+
+    const unsubscribe: TUnsubscribeFunction = (id) => {
+        const matchingSubscription = subscriptions.current?.[id];
+        if (matchingSubscription) matchingSubscription.unsubscribe();
+    };
 
     return (
-        <APIDataContext.Provider value={{ derivAPI: derivAPI.current }}>
+        <APIDataContext.Provider value={{ derivAPI: derivAPI.current, send, subscribe, unsubscribe }}>
             <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
         </APIDataContext.Provider>
     );
