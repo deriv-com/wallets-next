40163acea3fa6ac2a057a06828fac07f9c258fbe
yashim-deriv <yashim@regentmarkets.com>
2023-12-13 13:31:36 +0800

    feat: support multi subscription for exchange rates

--- src/api/base/use-exchange-rates.ts
+++ src/api/base/use-exchange-rates.ts
@@ -1,18 +1,38 @@
-import { useState } from "react";
-import { TSocketRequestPayload } from "../types/api.types";
+import { useRef, useState } from "react";
+import { TSocketRequestPayload, TSocketResponseData } from "../types/api.types";
 import useDerivAPI from "../lib/use-deriv-api";
 
 type TCurrencyPayload = NonNullable<TSocketRequestPayload<"exchange_rates">["target_currency"]>;
+type TCurrencyRateData = NonNullable<TSocketResponseData<"exchange_rates">["exchange_rates"]>["rates"];
 
 const useExchangeRates = <T extends TCurrencyPayload>() => {
     const { subscribe: _subscribe } = useDerivAPI();
-    const [status, setStatus] = useState;
+    const exchangeRatesSubscriptions = useRef<string[]>([]);
+    const [data, setData] = useState<Record<TCurrencyPayload, TCurrencyRateData>>();
 
     const subscribe = async (base_currency: T, target_currencies: T[]) => {
         target_currencies.forEach(async (c) => {
             const { id, subscription } = await _subscribe("exchange_rates", { base_currency, target_currency: c });
+            exchangeRatesSubscriptions.current.push(id);
+            subscription.subscribe((response) => {
+                const rates = response.exchange_rates?.rates;
+                if (rates) {
+                    setData((prev) => {
+                        const currentData = { ...(prev || {}) };
+                        if (currentData) {
+                            currentData[base_currency] = { ...currentData[base_currency], ...rates };
+                            return currentData;
+                        }
+                        return { [base_currency]: rates };
+                    });
+                }
+            });
         });
     };
+
+    const unsubscribe = () => {};
+
+    return { data, subscribe, unsubscribe };
 };
 
 export default useExchangeRates;
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,15 +1,21 @@
 import { useEffect } from "react";
 import useSingleSubscription from "../api/lib/use-single-subscription";
+import useExchangeRates from "../api/base/use-exchange-rates";
 
 const App = () => {
     const { isLoading: isR50Loading, data: data1, subscribe: subscribe1 } = useSingleSubscription("ticks");
     const { isLoading: isBTCLoading, data: data2, subscribe: subscribe2 } = useSingleSubscription("ticks");
     const isLoading = isR50Loading || isBTCLoading;
 
+    const { data, subscribe: multiSubscribe } = useExchangeRates();
+
     useEffect(() => {
         subscribe1({ ticks: "R_50" });
         subscribe2({ ticks: "cryBTCUSD" });
-    }, [subscribe1, subscribe2]);
+        multiSubscribe("USD", ["BTC", "ETH"]);
+        multiSubscribe("GBP", ["BTC", "ETH"]);
+        multiSubscribe("BTC", ["GBP", "ETH"]);
+    }, [multiSubscribe, subscribe1, subscribe2]);
 
     return (
         <div>
@@ -19,6 +25,7 @@ const App = () => {
                 <div>
                     <div>R50: {data1?.tick?.ask}</div>
                     <div>BTC: {data2?.tick?.ask}</div>
+                    <div>{JSON.stringify(data)}</div>
                 </div>
             )}
         </div>
