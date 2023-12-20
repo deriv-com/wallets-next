a4159dac24259b08004fc5356f82a67846ddf6e5
yashim-deriv <yashim@regentmarkets.com>
2023-12-20 19:23:39 +0800

    feat: add auth context

--- src/utils/constants/localstorage.constants.ts
+++ src/utils/constants/localstorage.constants.ts
@@ -1,5 +1,5 @@
-export const activeLoginid = "active_loginid";
-export const clientAccounts = "client.accounts";
+export const activeLoginid = "client.active_loginid";
+export const accountList = "client.account_list";
 export const configAppId = "config.app_id";
 export const configServerURL = "config.server_url";
 export const i18nLanguage = "i18n_language";
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export * as LocalStorageConstants from "./constants/localstorage.constants";
 export * as ServerContants from "./constants/server.constants";
 export * as CryptoUtils from "./crypto.utils";
+export * as LocalStorageUtils from "./localstorage.utils";
+export * as URLUtils from "./url.utils";
