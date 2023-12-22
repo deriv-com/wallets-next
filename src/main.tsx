9686a8cd8e9e9ae4afa844c5a8b792cb34b00b4c
yashim-deriv <yashim@regentmarkets.com>
2023-12-22 12:03:07 +0800

    chore: add quill-design & initial route config

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/app";
 import APIProvider from "./api/context/api-context";
 import AuthProvider from "./api/context/auth-context";
+import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
---
+++ src/routes/config.routes.ts
@@ -0,0 +1,5 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+
+const routeConfig: RouteObject[] = [];
+
+export const router = createBrowserRouter(routeConfig);
